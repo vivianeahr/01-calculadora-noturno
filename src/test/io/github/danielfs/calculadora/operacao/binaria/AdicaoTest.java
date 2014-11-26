0737a9352eb3960b19ee26caf160677ac0c8dc83
vivianeahr <vivianeoliveira.ti@hotmail.com>
2014-11-26 21:14:26 -0200

    Create AdicaoTest.java

---
+++ src/test/io/github/danielfs/calculadora/operacao/binaria/AdicaoTest.java
@@ -0,0 +1,25 @@
+package io.github.danielfs.calculadora.operacao.binaria;
+
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
+public class AdicaoTest
+{
+    private Adicao adicao;
+    
+    @Before
+    public void inicializar()
+    {
+      adicao = new adicao();
+    }
+    
+    @Test
+    public void deveriaRetornar5QuandoPassa2e3()
+    {
+      double valorEsperado = 5;
+      double valorRetornado = adicao.calcular(2, 3);
+      
+      assetEquals(valorEsperado, valorRetornado, 0);
+    }
+}
